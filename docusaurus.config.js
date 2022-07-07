2e121456b94b50435489d8d91f389e5aad458da8
shiwen <shiwen@4399.com>
2022-07-07 17:52:57 +0800

    修改baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://wenwenbo.github.io/',
-  baseUrl: '/blog/build/',
+  baseUrl: '/blog/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
